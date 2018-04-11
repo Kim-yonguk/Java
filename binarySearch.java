2d61e604ad9a61d4fe056f2a30699b67ceb7b91d
Kim-yonguk <1a1a11a@naver.com>
2018-04-11 17:05:13 +0900

    Sort

---
+++ binarySearch.java
@@ -0,0 +1,46 @@
+package sort;
+
+import java.util.Scanner;
+
+public class binarySearch {
+
+	
+	public static void main(String[] args) {
+		
+		String arr[]=new String[10];
+		
+		Scanner sc = new Scanner(System.in);
+		
+		for(int i=0; i<10; i++) {
+			arr[i]=sc.next();
+		}
+		sc.close();
+		
+		String str=sc.next();
+		
+		System.out.println(binary(arr,str));
+		
+	}
+	
+	public static int binary(String arr[],String str) {
+
+		int begin=0; 
+		int end=arr.length;
+		
+		while(begin<=end) {
+			int middle=(begin+end)/2;
+			int result=arr[middle].compareTo(str);
+			if(result==0) {
+				return middle;
+			}else if(result<0) {
+				begin=middle+1;
+			}else {
+				end=middle-1;
+			}
+		}
+		return -1;
+	}
+	
+	
+	
+}
---
+++ insertSort.java
@@ -0,0 +1,38 @@
+package sort;
+
+import java.util.Scanner;
+
+public class insertSort {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int n=sc.nextInt();
+		int arr[]=new int[n];
+		
+		for(int i=0; i<n; i++) {
+			arr[i]=sc.nextInt();
+		}
+		sc.close();
+		
+		insertSort(arr);
+		
+	}
+
+
+	public static void insertSort(int[] arr) {
+		for(int i=1; i<arr.length; i++) {
+			int tmp=arr[i];
+			int aux=i-1;
+			
+			while(aux>=0 && tmp<arr[aux]) {
+				arr[aux+1]=arr[aux];
+				aux--;
+			}
+			arr[aux+1]=tmp;
+		}
+		
+		for(int q:arr) {
+			System.out.println(q);
+		}
+	}
+}
